d16a4aa0c729bccb98f7d1876130e9ebe36562f6
Huy <thanhhuyn.0@gmail.com>
2020-10-21 13:07:25 +0700

    Add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BudgetComponent } from './budget/budget.component';
+import { ConvertComponent } from './convert/convert.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: BudgetComponent },
+  { path: 'budget', component: BudgetComponent },
+  { path: 'convert', component: ConvertComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BudgetComponent } from './budget/budget.component';
 import { AddFormComponent } from './add-form/add-form.component';
+import { ConvertComponent } from './convert/convert.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BudgetComponent,
-    AddFormComponent
+    AddFormComponent,
+    ConvertComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
